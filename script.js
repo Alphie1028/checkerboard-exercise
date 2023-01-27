9559289dc41ce2a4777a67de739dcdcd99de934f
Alphie Brennan <alphie1028@gmail.com>
2023-01-27 09:23:33 -0500

    basic checkerboard complete

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ for(let i = 0; i < 81; i++){
     let tile = document.createElement('div');
         tile.style.minWidth = '11.1%';
         tile.style.paddingBottom = '11.1%';
-        tile.style.float = 'left';
+        tile.style.float ='left';
 
         if(i % 2 === 0){
             tile.style.backgroundColor = 'black';
         }else{
             tile.style.backgroundColor = 'red';
         }
-        document.body.appendChild(tile);
+        document.body.appendChild(tile); 
 }
