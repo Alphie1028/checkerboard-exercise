4fc412927fee6bbff6b0d14686960039c2b834c9
Alphie Brennan <alphie1028@gmail.com>
2023-01-27 08:59:57 -0500

    basic black/red checkerboard

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-// Your JS goes here
+for(let i = 0; i < 81; i++){
+    let tile = document.createElement('div');
+        tile.style.minWidth = '11.1%';
+        tile.style.paddingBottom = '11.1%';
+        tile.style.float = 'left';
+
+        if(i % 2 === 0){
+            tile.style.backgroundColor = 'black';
+        }else{
+            tile.style.backgroundColor = 'red';
+        }
+        document.body.appendChild(tile);
+}
